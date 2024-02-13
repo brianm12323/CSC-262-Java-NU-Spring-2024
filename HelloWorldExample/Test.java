353ca6c208c049197df8ae5fe9a308faffd22657
brianm12323 <159477280+brianm12323@users.noreply.github.com>
2024-02-13 18:59:22 +0000

    commit

---
+++ HelloWorldExample/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        // Prints Hello World to Terminal Screen
+        System.out.println("This is a test that I can create a file from here and it will automatically save to the repository!");
+    }
+}
